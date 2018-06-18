ccd2254039003d3475225cdd4fcd0fe28bc98b1e
Shiang Lee <e94016021@gmail.com>
2018-06-18 20:17:08 +0800

    full npy

---
+++ classiceal.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+
+
+def genre_id2top_id(input_id, top):
+    g_id = top.genre_id
+    top_id = top.top_level
+    check = -1
+    for work in range(len(g_id)):
+        if input_id == g_id[work]:
+            check = top_id[work]
+    return check
+
+
+def genre_id2genre_title(input_id, top):
+    # target_id = top.values[0][0]
+    # target_title = top.values[0][3]
+    check = str(-1)
+    for work in range(len(top.genre_id)):
+        if input_id == top.values[work][0]:
+            check = top.values[work][3]
+    return check
+
+
+if __name__ == '__main__':
+    tracks = pd.read_csv('fma_metadata/raw_tracks.csv')
+    genres = pd.read_csv('fma_metadata/genres.csv')
+
+    classical_id = 5
+    country_id = 9
+    spoken_id = 20
+    blues_id = 3
+    soul_RnB_id = 14
+
+    target_id_list = [5, 9, 20, 3, 14]
+    try:
+
+        for target_genre_id in target_id_list:
+            try:
+                target_genre_id_top = genre_id2top_id(target_genre_id, genres)
+
+                print("~\n~\n~ ~ deal with\n~\n~", genre_id2genre_title(target_genre_id_top, genres))
+
+                target_genre_tracks = []
+                no_genre_cnt = 0
+                try:
+
+                    for track_id in range(tracks.shape[0]):
+                        try:
+                            # test if without genre
+                            test = tracks.track_genres[track_id]
+                            if type(test) is float:
+                                print("track_id:%d" % track_id, "no genre")
+                                no_genre_cnt += 1
+                                continue
+
+                            genre_id = int(tracks.track_genres[track_id].split('\'')[3])
+                            genre_id_top = genre_id2top_id(genre_id, genres)
+
+                            if genre_id_top == target_genre_id_top:
+                                print(genre_id2genre_title(genre_id, genres), end=' -> ')
+                                print(genre_id2genre_title(genre_id_top, genres))
+                                target_genre_tracks.append(track_id)
+
+                        except Exception as e:
+                            print("id:%d" % track_id, "inner ERROR :", e)
+                    print("*")
+                    print(target_genre_tracks)
+                    string = genre_id2genre_title(target_genre_id_top, genres) + ".npy"
+                    np.save(string, target_genre_tracks)
+
+                    print("count of no genre's file", no_genre_cnt)
+
+                except Exception as e:
+                    print("outer ERROR", e)
+            except Exception:
+                print("a", Exception)
+    except Exception:
+        print("genre_id ERROR :", Exception)
--- get_list.py
+++ get_list.py
@@ -2,20 +2,14 @@ import pandas as pd
 import numpy as np
 
 if __name__ == "__main__":
-    pop_list =np.load("Pop.npy")
-    print("pop",pop_list.shape[0])
-    pop_list =np.load("Folk.npy")
-    print("Folk.npy",pop_list.shape[0])
-    pop_list =np.load("Hip-Hop.npy")
-    print("Hip-Hop.npy",pop_list.shape[0])
-    pop_list =np.load("Instrumental.npy")
-    print("Instrumental.npy",pop_list.shape[0])
-    pop_list =np.load("International.npy")
-    print("International.npy",pop_list.shape[0])
-    pop_list =np.load("Jazz.npy")
-    print("Jazz.npy",pop_list.shape[0])
-    pop_list =np.load("Rock.npy")
-    print("Rock.npy",pop_list.shape[0])
-    pop_list =np.load("Jazz.npy")
-    print(".")
 
+    names = ["Blues.npy", "Classical.npy", "Country.npy", "Electronic_fake.npy",
+             "ex_chip.npy", "Experimental.npy", "Folk.npy", "Hip-Hop.npy",
+             "Instrumental.npy", "International.npy", "Jazz.npy",
+             "Pop.npy", "Rock.npy", "Soul-RnB.npy", "Spoken.npy", "old_time.npy", "Easy Listening.npy"]
+
+    for name in names:
+        elec_list = np.load(name)
+        print(name.split(".npy")[0], "cnt =", elec_list.shape[0])
+
+    print(".")
